4ace36db5c812722b3d6903569c2c8080c8b0f84
Gabriela Guevara <agabrielaguevaraa@hotmail.com>
2024-04-08 22:29:31 -0500

    Agregamos un JSON con la información de la API

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import prueba from './routers/pruebas.js'
 const app = express()
-
 app.use('/prueba', prueba)
 app.get('/', (req, res) => {
-   res.send('hola semillero')
+   let result = {
+      appname: "ApiGlab",
+      version: "1.0.0",
+      developers: ["Gabriela", "Juanes", "Javi", "Sebas"],
+      description: "Estamos crando la API en el semillero aplicando los conocimientos que hemos adquirido en la clase del día lunes 08/04"
+   }
+   res.send(result)
 })
 const port = process.env.PORT || 3000
 app.listen(port, () => {
